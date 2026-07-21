f99c136f68a01f46f9b26c9e6146cb3fc09db3ad
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2026-07-21 15:07:31 -0500

    Add tying animation with typedjs

---
+++ assets/js/typing_animation.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#typing-text", {
+  strings: [
+    "Learn how to use TCP for timekeeping",
+    "Complete required student hiring forms (e.g, W-4, I-9, etc.)",
+    "Check out the payroll process for the next pay date, and more.",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
