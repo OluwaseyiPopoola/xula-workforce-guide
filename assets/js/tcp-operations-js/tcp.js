fa93e9564baffebc67ed24172eb4e70a2e3d35a6
OluwaseyiPopoola <oluwaseyipopoola49@gmail.com>
2026-08-05 12:01:57 -0500

    Add data for each tcp operation and step for dynamic rendering

---
+++ assets/js/tcp-operations-js/tcp.js
@@ -0,0 +1,178 @@
+// tcp.js
+
+// Part 1: Centralized Data Structure
+const tcpOperationsData = {
+  "clock-in": {
+    title: "How To Clock In",
+    steps: [
+      {
+        title: "Enter ID",
+        description: "Enter your employee ID number in the ID Number Field",
+        image: null
+      },
+      {
+        title: "Clock In",
+        description: "Press the Green 'Clock In' button",
+        image: null
+      },
+      {
+        title: "Confirm Name",
+        description: "Confirm your correct first and last name show on the screen",
+        image: null
+      },
+      {
+        title: "Continue",
+        description: "Press the 'Continue' button",
+        image: null
+      },
+      {
+        title: "Select Job Code",
+        description: "Select the Job Code/Position you will be working if prompted",
+        image: null
+      },
+      {
+        title: "Continue",
+        description: "Press the 'Continue' button",
+        image: null
+      },
+      {
+        title: "Confirm Operation",
+        description: "Press the 'OK' button on the clock operation confirmation screen",
+        image: null
+      },
+      {
+        title: "Success",
+        description: "You are now Clocked In!",
+        image: null
+      }
+    ]
+  },
+  
+  "leave-break": {
+    title: "How To Leave on Break",
+    steps: [
+      {
+        title: "Enter ID",
+        description: "Enter your employee ID number in the ID Number Field",
+        image: null
+      },
+      {
+        title: "Leave on Break",
+        description: "Press the White 'Leave on Break' button",
+        image: null
+      },
+      {
+        title: "Confirm Name",
+        description: "Confirm your correct first and last name show on the clock",
+        image: null
+      },
+      {
+        title: "Continue",
+        description: "Press the 'Continue' button",
+        image: null
+      },
+      {
+        title: "Confirm Operation",
+        description: "Press the 'OK' button on the clock operation confirmation screen",
+        image: null
+      },
+      {
+        title: "Success",
+        description: "You are now On Break!",
+        image: null
+      }
+    ]
+  },
+
+  "return-break": {
+    title: "How To Return From Break",
+    steps: [
+      {
+        title: "Enter ID",
+        description: "Enter your employee ID number in the ID Number Field",
+        image: null
+      },
+      {
+        title: "Return From Break",
+        description: "Press the White 'Return From Break' button",
+        image: null
+      },
+      {
+        title: "Confirm Name",
+        description: "Confirm your correct first and last name show on the clock",
+        image: null
+      },
+      {
+        title: "Continue",
+        description: "Press the 'Continue' button",
+        image: null
+      },
+      {
+        title: "Select Job Code",
+        description: "Select the Job Code/Position you will be working if prompted",
+        image: null
+      },
+      {
+        title: "Continue",
+        description: "Press the 'Continue' button",
+        image: null
+      },
+      {
+        title: "Confirm Operation",
+        description: "Press the 'OK' button on the clock operation confirmation screen",
+        image: null
+      },
+      {
+        title: "Success",
+        description: "You are now Back From Your Break!",
+        image: null
+      }
+    ]
+  },
+
+  "change-position": {
+    title: "How To Change Position/Job Code",
+    steps: [
+      {
+        title: "Enter ID",
+        description: "Enter your employee ID number in the ID Number Field",
+        image: null
+      },
+      {
+        title: "Change Job Code",
+        description: "Press the 'Change Job Code' button",
+        image: null
+      },
+      {
+        title: "Confirm Name",
+        description: "Confirm your correct first and last name show on the clock",
+        image: null
+      },
+      {
+        title: "Continue",
+        description: "Press the 'Continue' button",
+        image: null
+      },
+      {
+        title: "Select Next Job",
+        description: "Select the Job Code/Position you will be working next",
+        image: null
+      },
+      {
+        title: "Continue",
+        description: "Press the 'Continue' button",
+        image: null
+      },
+      {
+        title: "Confirm Operation",
+        description: "Press the 'OK' button on the clock operation confirmation screen",
+        image: null
+      },
+      {
+        title: "Success",
+        description: "You have changed your Job Code!",
+        image: null
+      }
+    ]
+  }
+};
